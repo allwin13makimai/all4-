4d8ec701ec9895029d020fe679c9ac78d458cd36
allwin <36722666+allwin13makimai@users.noreply.github.com>
2018-07-23 12:45:34 +0530

    Create win7.py

---
+++ win7.py
@@ -0,0 +1,10 @@
+b=int(input("Enter the number1:"))
+c=int(input("Enter the number2:"))
+d=int(input("Enter the number3:"))
+if(b>c):
+  if(b>d):
+    print(b)
+elif(c>d):
+  print(c)
+else:
+  print(d)
