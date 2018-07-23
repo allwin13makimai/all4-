598c85a88e8591f1396d6ef804d2510a2c1a5713
allwin <36722666+allwin13makimai@users.noreply.github.com>
2018-07-23 13:55:53 +0530

    Create win14.c

---
+++ win14.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+ int n,r,ans;
+printf("enthr the num:");
+scanf("%d",&n);
+printf("enter the power:");
+scanf("%d",&r);
+ans=pow(n,r);
+printf("%d",ans);
+return 0;
+}
